f29faa25339ac89fdff3a81fbaec3e526f2617ec
Ozgun <ozgun.abanoz@gmail.com>
2019-01-30 10:43:43 +0300

    add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+var socket = io();
+
+socket.on('connect', function () {
+
+    console.log('connected to server');
+
+    socket.emit('createMessage', {
+
+        from: 'all',
+        text: ':-)'
+
+    });
+
+});
+
+socket.on('disconnect', function () {
+
+    console.log('disconnected from server');
+
+});
+
+socket.on('newMessage', function (message) {
+
+    console.log(message);
+
+});
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -14,6 +15,28 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 
     console.log('new user connected');
+
+    socket.emit('newEmail', {
+
+        from: 'mike@example.com',
+        text: 'ayoo',
+        createdAt: 124
+
+    });
+
+    socket.on('createMessage', (newMsg) => {
+
+        console.log(newMsg);
+
+    });
+
+    socket.emit('newMessage', {
+
+        from: 'ozgun',
+        text: 'yarrak yemisin sen',
+        createdAt: new Date().getTime()
+
+    });
 
     socket.on('disconnect', () => {
 
