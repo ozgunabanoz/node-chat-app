1cc42795123e336000307ec78c6693e8d3ed5060
Ozgun <ozgun.abanoz@gmail.com>
2019-02-01 09:04:50 +0300

    Added users class and update user list event

---
+++ server/utils/validation.test.js
@@ -0,0 +1,30 @@
+const expect = require('expect');
+const {isRealString} = require('./validation');
+
+describe('isRealString', () => {
+
+    it('should reject non-string values', () => {
+
+         var testVar = 5;
+
+         expect(isRealString(testVar)).toBe(false);
+
+    });
+
+    it('should reject strings with only spaces', () => {
+
+        var testVar = '       ';
+
+        expect(isRealString(testVar)).toBe(false);
+
+    });
+
+    it('should allow strings with non-space characters', () => {
+
+        var testVar = '    sdsd   ';
+
+        expect(isRealString(testVar)).toBe(true);
+
+    });
+
+});
